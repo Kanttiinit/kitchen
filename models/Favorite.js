a28b2f2030d948b8c3866955b62e3e84b3b7df71
Max Huttunen <max.huttunen@gmail.com>
2016-03-09 17:46:55 +0200

    add model for favorites

---
+++ models/Favorite.js
@@ -0,0 +1,10 @@
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('Favorite', {
+		id: {type: DataTypes.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true},
+      name: DataTypes.STRING,
+      regexp: DataTypes.STRING,
+      icon: DataTypes.STRING
+	}, {
+		classMethods: {}
+	});
+};
