977f68ba11e9a52cfc729d92565d4c704b57e2fa
Max Huttunen <max.huttunen@gmail.com>
2016-04-02 15:03:18 +0300

    add parser for hämäläinen osakunta

---
+++ parser/parsers/hamis.js
@@ -0,0 +1,33 @@
+const xml2js = require('xml2js').parseString;
+const utils = require('../utils');
+const moment = require('moment');
+
+function parseXml(xml) {
+   return new Promise((resolve, reject) => {
+      xml2js(xml, function(err, data) {
+         if (err)
+            reject(err);
+         resolve(data);
+      });
+   });
+}
+
+module.exports = {
+   pattern: /www\.hys\.net\/ruokalista\.xml/,
+   parser: function(url) {
+      return utils.text(url)
+      .then(xml => parseXml(xml))
+      .then(data => {
+         return data.rss.channel[0].item
+         .map(item => {
+            const date = moment(item.title[0].split(' ')[1], 'DD.MM');
+            return {
+               date: date.toDate(),
+               day: date.format('YYYY-MM-DD'),
+               courses: item.description[0].split(' ,<br />\r\n')
+               .map(_ => ({title: _, properties: []})),
+            };
+         });
+      });
+   }
+}
