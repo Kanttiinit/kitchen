ee64004967743bc928214553a82663fb7701dec1
Max Huttunen <max.huttunen@gmail.com>
2016-04-13 15:20:53 +0300

    draw images with canvas

--- image-generator/index.js
+++ image-generator/index.js
@@ -1,10 +1,7 @@
-const webshot = require('webshot');
-const jade = require('jade');
 const models = require('../models');
 const moment = require('moment');
 const fs = require('fs');
-
-const imageTemplate = jade.compileFile(__dirname + '/restaurant-image.jade');
+const Canvas = require('canvas');
 
 function getImage(restaurantId, date) {
    date = moment(date).format('YYYY-MM-DD');
@@ -33,24 +30,59 @@ function generateImage(restaurantId, date, filename) {
       ]
    })
    .then(menu => new Promise((resolve, reject) => {
-      webshot(
-         imageTemplate({
-            courses: menu.courses,
-            restaurant: menu.Restaurant,
-            date: moment(menu.date).format('DD.MM.YYYY')
-         }),
-         filename,
-         {
-            siteType: 'html',
-            screenSize: {width: 500, height: 300}
-         },
-         function(err) {
-            if (err)
-               reject(err);
-            else
-               resolve();
-         }
-      );
+      const margin = 10;
+      const spacing = {
+         huge: 16,
+         big: 12,
+         small: 8,
+         tiny: 4
+      };
+      const fontSize = {
+         title: 30,
+         date: 14,
+         course: 13
+      };
+
+      const height = margin * 2 + fontSize.title + fontSize.date + spacing.small + (menu.courses.length + 1) * (fontSize.course + spacing.small);
+      const canvas = new Canvas(500, height);
+      const ctx = canvas.getContext('2d');
+
+      var y = margin + fontSize.title - 3;
+
+      ctx.fillStyle = '#eee';
+      ctx.fillRect(0, 0, 500, height);
+
+      ctx.fillStyle = '#139180';
+      ctx.fillRect(0, 0, 500, fontSize.title + fontSize.date + spacing.small + margin * 2);
+      ctx.font = fontSize.title + 'px Helvetica';
+      ctx.fillStyle = '#fff';
+      ctx.fillText(menu.Restaurant.name, margin, y);
+
+      ctx.font = fontSize.date + 'px Helvetica';
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+      y += fontSize.date + spacing.small;
+      ctx.fillText(moment(menu.date).format('D.M.YYYY'), margin, y);
+
+      ctx.font = '12px Helvetica';
+      y += fontSize.course + spacing.big + margin;
+      menu.courses.forEach(course => {
+         ctx.fillStyle = '#000';
+         ctx.fillText('• ' + course.title, margin, y);
+         var x = ctx.measureText('• ' + course.title).width + spacing.huge;
+         ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+         course.properties.forEach(property => {
+            ctx.fillText(property, x, y);
+            x += ctx.measureText(property).width + spacing.tiny;
+         });
+         y += fontSize.course + spacing.small;
+      });
+
+      fs.writeFile(filename, canvas.toBuffer(), err => {
+         if (err)
+            reject(err);
+         else
+            resolve(filename);
+      });
    }));
 }
 
