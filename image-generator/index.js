bcf7bcb424607951ec3b082af946afe3657175f6
Max Huttunen <max.huttunen@gmail.com>
2016-04-18 00:05:32 +0300

    image generator tweaks

--- image-generator/index.js
+++ image-generator/index.js
@@ -2,6 +2,19 @@ const models = require('../models');
 const moment = require('moment');
 const Canvas = require('canvas');
 
+const margin = 10;
+const spacing = {
+   huge: 16,
+   big: 12,
+   small: 8,
+   tiny: 4
+};
+const fontSize = {
+   title: 26,
+   date: 16,
+   course: 14
+};
+
 function generateImage(restaurantId, date) {
    console.log(restaurantId, date);
    return models.Menu.findOne({
@@ -14,41 +27,28 @@ function generateImage(restaurantId, date) {
       ]
    })
    .then(menu => {
-      console.log(menu);
-      const margin = 10;
-      const spacing = {
-         huge: 16,
-         big: 12,
-         small: 8,
-         tiny: 4
-      };
-      const fontSize = {
-         title: 30,
-         date: 14,
-         course: 13
-      };
-
+      const width = 500;
       const height = margin * 2 + fontSize.title + fontSize.date + spacing.small + (menu.courses.length + 1) * (fontSize.course + spacing.small);
-      const canvas = new Canvas(500, height);
+      const canvas = new Canvas(width, height);
       const ctx = canvas.getContext('2d');
 
       var y = margin + fontSize.title - 3;
 
       ctx.fillStyle = '#eee';
-      ctx.fillRect(0, 0, 500, height);
+      ctx.fillRect(0, 0, width, height);
 
       ctx.fillStyle = '#139180';
-      ctx.fillRect(0, 0, 500, fontSize.title + fontSize.date + spacing.small + margin * 2);
+      ctx.fillRect(0, 0, width, fontSize.title + fontSize.date + spacing.small + margin * 2);
       ctx.font = fontSize.title + 'px Helvetica';
       ctx.fillStyle = '#fff';
       ctx.fillText(menu.Restaurant.name, margin, y);
 
       ctx.font = fontSize.date + 'px Helvetica';
-      ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.7)';
       y += fontSize.date + spacing.small;
-      ctx.fillText(moment(menu.date).format('D.M.YYYY'), margin, y);
+      ctx.fillText(moment(menu.date).format('ddd D.M.YYYY'), margin, y);
 
-      ctx.font = '12px Helvetica';
+      ctx.font = fontSize.course + 'px Helvetica';
       y += fontSize.course + spacing.big + margin;
       menu.courses.forEach(course => {
          ctx.fillStyle = '#000';
