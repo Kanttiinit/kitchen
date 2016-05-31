c7a176a9dad8dad0014cc63dcfb32bd768a2e657
Max Huttunen <max.huttunen@gmail.com>
2016-06-01 00:19:30 +0300

    new admin

--- admin/src/AdminInterface.js
+++ admin/src/AdminInterface.js
@@ -1,166 +1,83 @@
 import React from 'react';
 import http from 'axios';
 import Table from './Table';
-import Form from './Form';
-import {Input} from 'react-bootstrap';
 import moment from 'moment';
+import Ace from 'react-ace';
+
+import 'brace/mode/json';
+import 'brace/theme/github';
 
 export default class AdminInterface extends React.Component {
    constructor() {
       super();
-
-      this.state = {
-         areas: [],
-         restaurants: [],
-         favorites: []
-      };
+      this.state = {items: []};
    }
-   componentDidMount() {
-      this.updateAreas();
-      this.updateRestaurants();
-      this.updateFavorites();
+   getBasePath() {
+      return '/' + this.props.model.name.toLowerCase();
    }
-   updateAreas() {
-      http.get('/areas').then(response => this.setState({areas: response.data}));
+   save() {
+      const item = JSON.parse(this.state.editorContent);
+      http.put(this.getBasePath() + '/' + item.id, item)
+      .then(r => console.log(r));
    }
-   updateRestaurants() {
-      http.get('/restaurants').then(response => this.setState({restaurants: response.data}));
-   }
-   updateFavorites() {
-      http.get('/favorites').then(response => this.setState({favorites: response.data}));
-   }
-   logOut() {
-      http.post('/admin/logout').then(response => {
-         this.props.setLoggedIn(false);
+   edit(item) {
+      this.setState({
+         editorContent: JSON.stringify(item, null, '  '),
+         mode: 'editing'
       });
    }
-   updateMenus() {
-      this.setState({updatingRestaurants: true});
-      http.post('/restaurants/update')
-      .then(_ => this.setState({updatingRestaurants: false}));
+   delete(item) {
+      if (confirm('Are you sure?')) {
+         http.delete(this.getBasePath() + '/' + item.id)
+         .then(response => this.update());
+      }
    }
-   edit(type, item) {
-      this.refs[type + 'Form'].prepareForEditing(item);
-   }
-   delete(type, item) {
-      if (confirm('Are you sure?'))
-         http.delete('/' + type + 's/' + item.id)
-         .then(response => {
-            this.updateAreas();
-            this.updateRestaurants();
-            this.updateFavorites();
-         });
-   }
-   renderAreaItem(area) {
+   renderItem(item) {
       return (
          <tr>
-            <td>{area.id}</td>
-            <td>{area.name}</td>
-            <td>{area.image}</td>
-            <td>{area.latitude}, {area.longitude}</td>
-            <td>{area.locationRadius}</td>
+            {Object.keys(this.props.model.tableFields).map(key =>
+            <td key={key}>{item[key]}</td>
+            )}
             <td>
-               <button onClick={this.edit.bind(this, 'area', area)} className="btn btn-xs btn-warning">Edit</button>&nbsp;
-               <button onClick={this.delete.bind(this, 'area', area)} className="btn btn-xs btn-danger">Delete</button>
-            </td>
-         </tr>
-      );
-   }
-   renderRestaurantItem(restaurant) {
-      const formatUrl = (url, date) => {
-      	date = date || moment();
-      	return url
-      		.replace('%year%', date.format('YYYY'))
-      		.replace('%month%', date.format('MM'))
-      		.replace('%day%', date.format('DD'));
-      };
-
-      return (
-         <tr>
-            <td>{restaurant.id}</td>
-            <td>{restaurant.Area ? restaurant.Area.name : 'none'}</td>
-            <td>{restaurant.name}</td>
-            <td>{restaurant.image}</td>
-            <td>{restaurant.url ? <a href={restaurant.url} target="_blank">Open</a> : null}</td>
-            <td>{restaurant.menuUrl ? <a href={formatUrl(restaurant.menuUrl)} target="_blank">Open</a> : null}</td>
-            <td>{restaurant.openingHours && restaurant.openingHours.length ? 'defined' : 'undefined'}</td>
-            <td>{restaurant.address}</td>
-            <td>
-               {restaurant.latitude && restaurant.longitude ?
-               <a href={'https://www.google.fi/maps/place/' + restaurant.latitude + ',' + restaurant.longitude}>
-                  {restaurant.latitude}, {restaurant.longitude}
-               </a>
-               : null}
-            </td>
-            <td>
-               &nbsp;<button onClick={this.edit.bind(this, 'restaurant', restaurant)} className="btn btn-xs btn-warning">Edit</button>&nbsp;
-               <button onClick={this.delete.bind(this, 'restaurant', restaurant)} className="btn btn-xs btn-danger">Delete</button>
-            </td>
-         </tr>
-      );
-   }
-   renderFavoriteItem(favorite) {
-      return (
-         <tr>
-            <td>{favorite.id}</td>
-            <td>{favorite.name}</td>
-            <td>{favorite.regexp}</td>
-            <td>{favorite.icon}</td>
-            <td>
-               &nbsp;<button onClick={this.edit.bind(this, 'favorite', favorite)} className="btn btn-xs btn-warning">Edit</button>&nbsp;
-               <button onClick={this.delete.bind(this, 'favorite', favorite)} className="btn btn-xs btn-danger">Delete</button>
+               &nbsp;<button onClick={this.edit.bind(this, item)} className="btn btn-xs btn-warning">Edit</button>&nbsp;
+               <button onClick={this.delete.bind(this, item)} className="btn btn-xs btn-danger">Delete</button>
             </td>
          </tr>
       );
    }
    render() {
+      const {model, items} = this.props;
+      const {mode, editorContent} = this.state;
+
+      if (!items) {
+         return <p>Loading...</p>;
+      }
+
+      const headers = Object.keys(model.tableFields).map(key => model.tableFields[key]).concat('');
+
       return (
          <div>
-            <button className="btn btn-warning pull-right" onClick={this.logOut.bind(this)} style={{marginLeft: '1em'}}>Log out</button>
-            <button className="btn btn-primary pull-right" disabled={this.state.updatingRestaurants} onClick={this.updateMenus.bind(this)}>{this.state.updatingRestaurants ? 'Updating...' : 'Update menus'}</button>
-            <h1>Areas</h1>
-            <Form ref="areaForm" type="area" onCreated={this.updateAreas}>
-               <Input type="text" label="Name" name="name" />
-               <Input type="text" label="Image" name="image" />
-               <Input type="number" label="Latitude" name="latitude" step="0.0000001" />
-               <Input type="number" label="Longitude" name="longitude" step="0.0000001" />
-               <Input type="number" label="Location radius (in kilometers)" name="locationRadius" step="0.1" min="0.5" />
-            </Form>
+            <h1>{model.name}</h1>
+            {mode &&
+            <div className="panel panel-primary">
+               <div className="panel-heading">Editing</div>
+               <div className="panel-body">
+                  <Ace
+                     width='100%'
+                     height='200px'
+                     onChange={editorContent => this.setState({editorContent})}
+                     value={editorContent}
+                     theme="github"
+                     mode="json" />
+                  <button onClick={this.save.bind(this)} className="btn btn-primary">Save</button>&nbsp;
+                  <button onClick={() => this.setState({mode: undefined})} className="btn btn-warning">Cancel</button>
+               </div>
+            </div>
+            }
             <Table
-               headers={['ID', 'Name', 'Image', 'Location', 'Location radius', '']}
-               data={this.state.areas}
-               sortBy="name">
-               {this.renderAreaItem.bind(this)}
-            </Table>
-            <h1>Restaurants</h1>
-            <Form ref="restaurantForm" type="restaurant" onCreated={this.updateRestaurants.bind(this)}>
-               <Input type="text" label="Name" name="name" />
-               <Input type="text" label="Image" name="image" />
-               <Input type="url" label="URL" name="url" />
-               <Input type="url" label="Menu URL" name="menuUrl" />
-               <Input type="text" label="Opening Hours" name="openingHours" />
-               <Input type="text" label="Address" name="address" />
-               <Input type="number" label="Latitude" name="latitude" step="0.0000001" />
-               <Input type="number" label="Longitude" name="longitude" step="0.0000001" />
-               <Input type="select" label="Area" name="AreaId">
-                  {this.state.areas.map(area => <option key={area.id} value={area.id}>{area.name}</option>)}
-               </Input>
-            </Form>
-            <Table
-               headers={['ID', 'Area', 'Name', 'Image', 'URL', 'Menu URL', 'Opening Hours', 'Address', 'Location', '']}
-               data={this.state.restaurants}>
-               {this.renderRestaurantItem.bind(this)}
-            </Table>
-            <h1>Favorites</h1>
-            <Form ref="favoriteForm" type="favorite" onCreated={this.updateFavorites.bind(this)}>
-               <Input type="text" label="Name" name="name" />
-               <Input type="text" label="Regular expression" name="regexp" />
-               <Input type="text" label="Icon" name="icon" />
-            </Form>
-            <Table
-               headers={['ID', 'Name', 'Regular Expression', 'Icon', '']}
-               data={this.state.favorites}>
-               {this.renderFavoriteItem.bind(this)}
+               headers={headers}
+               data={items}>
+               {this.renderItem.bind(this)}
             </Table>
          </div>
       );
--- admin/src/BaseView.js
+++ admin/src/BaseView.js
@@ -4,6 +4,21 @@ import http from 'axios';
 import LoginForm from './LoginForm';
 import AdminInterface from './AdminInterface';
 
+const models = [
+   {
+      name: 'Areas',
+      tableFields: {id: 'ID', name: 'Name'}
+   },
+   {
+      name: 'Restaurants',
+      tableFields: {id: 'ID', name: 'Name'}
+   },
+   {
+      name: 'Favorites',
+      tableFields: {id: 'ID', name: 'Name', regexp: 'Regular Expression', icon: 'Icon'}
+   }
+];
+
 class BaseView extends React.Component {
    constructor() {
       super();
@@ -15,13 +30,51 @@ class BaseView extends React.Component {
    }
    setLoggedIn(loggedIn) {
       this.setState({loggedIn});
+      if (loggedIn) {
+         this.changeModel(models[0]);
+      }
+   }
+   logOut() {
+      http.post('/admin/logout').then(response => {
+         this.setLoggedIn(false);
+      });
+   }
+   updateMenus() {
+      this.setState({updatingRestaurants: true});
+      http.post('/restaurants/update')
+      .then(_ => this.setState({updatingRestaurants: false}));
+   }
+   changeModel(model) {
+      http.get('/' + model.name.toLowerCase())
+      .then(response => {
+         this.setState({
+            currentModel: model,
+            items: response.data
+         });
+      });
    }
    render() {
       if (this.state.loggedIn === undefined)
          return null;
 
-      if (this.state.loggedIn)
-         return <AdminInterface setLoggedIn={this.setLoggedIn.bind(this)} />;
+      if (this.state.loggedIn) {
+         const {currentModel, items} = this.state;
+         return (
+            <div>
+               <br />
+               <button className="btn btn-warning pull-right" onClick={this.logOut.bind(this)} style={{marginLeft: '1em'}}>Log out</button>
+               <button className="btn btn-primary pull-right" disabled={this.state.updatingRestaurants} onClick={this.updateMenus.bind(this)}>{this.state.updatingRestaurants ? 'Updating...' : 'Update menus'}</button>
+               <ul className="nav nav-tabs">
+                  {models.map(m =>
+                  <li key={m.name} className={m === currentModel ? 'active' : ''}>
+                     <a href="#" onClick={this.changeModel.bind(this, m)}>{m.name}</a>
+                  </li>
+                  )}
+               </ul>
+               <AdminInterface model={currentModel} items={items} />
+            </div>
+         );
+      }
 
       return <LoginForm setLoggedIn={this.setLoggedIn.bind(this)} />;
    }
