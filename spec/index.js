d4fbce8452bad43148bdd12f47a6dc2398854a77
Max Huttunen <max.huttunen@gmail.com>
2016-06-05 22:25:48 +0300

    the tests are alive

---
+++ spec/index.js
@@ -0,0 +1,73 @@
+const chai = require('chai');
+const request = require('supertest');
+
+const areaSchema = require('./schema/area.json');
+const restaurantSchema = require('./schema/restaurant.json');
+
+chai.use(require('chai-json-schema'));
+chai.tv4.addSchema('restaurant.json', restaurantSchema);
+chai.tv4.addSchema('menu.json', require('./schema/menu.json'));
+
+const expect = chai.expect;
+
+function generateListSchema(schema) {
+   return {
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "type": "array",
+      "items": schema
+   };
+}
+
+function basicRequest(endpoint, statusCode = 200) {
+   return new Promise((resolve, reject) => {
+      request('http://localhost:3000')
+      .get(endpoint)
+      .expect(statusCode)
+      .end(function(err, res) {
+         if (err) return reject(err);
+         resolve(res);
+      });
+   });
+}
+
+function validateEndpoint(endpoint, schema) {
+   return basicRequest(endpoint)
+   .then(res =>
+      expect(JSON.parse(res.text)).to.be.jsonSchema(schema)
+   );
+}
+
+describe('API', function() {
+   describe('/areas', function() {
+      it('is correctly formed', () =>
+         validateEndpoint('/areas', generateListSchema(areaSchema))
+      );
+   });
+
+   describe('/restaurants', function() {
+      it('is correctly formed', () =>
+         validateEndpoint('/restaurants', generateListSchema(restaurantSchema))
+      );
+   });
+
+   describe('/restaurants/1/menu', function() {
+      it('responds with json', () =>
+         validateEndpoint('/restaurants/1/menu', restaurantSchema)
+      );
+
+      it('responds with html', () =>
+         basicRequest('/restaurants/1/menu.html')
+         .then(res => {
+            expect(res.headers['content-type']).to.match(/text\/html/);
+         })
+      );
+
+      it('responds with image', function() {
+         this.timeout(5000);
+         return basicRequest('/restaurants/1/menu.png', 302)
+         .then(res => {
+            expect(res.headers['location']).to.match(/amazonaws\.com/);
+         });
+      });
+   });
+});
