5f1ad905c02c5704bed950c1000065e4a69a84fa
Max Huttunen <max.huttunen@gmail.com>
2016-06-07 20:03:03 +0300

    improve the worker

--- parser/worker.js
+++ parser/worker.js
@@ -1,50 +1,64 @@
 const models = require('../models');
-const parse = require('../parser');
+const parse = require('./index');
+
+const langs = ['fi', 'en'];
 
 function updateMenu(restaurant) {
-   return parse(restaurant.menuUrl)
-   .then(menus => {
-      console.log('\tFound ' + menus.length + ' days of menues.');
-      return Promise.all(
-         menus.map(menu =>
-            models.Menu.findOne({
-               where: {
-                  day: menu.day,
-                  RestaurantId: restaurant.id
-               }
-            })
-            .then(existing => {
-               if (existing)
-                  return existing.update({courses_i18n: menu.courses});
-
-               return models.Menu.create({
-                  day: menu.day,
-                  RestaurantId: restaurant.id,
-                  courses_i18n: menu.courses
-               });
-            })
-         )
-      );
-   });
-}
-
-function worker(restaurants, i) {
-   i = i || 0;
-   const restaurant = restaurants[i];
-
-   if (restaurant) {
-      console.log('Processing ' + (i + 1) + '/' + restaurants.length + ' ' + restaurant.name_i18n.fi);
-      return updateMenu(restaurant).then(() => worker(restaurants, i + 1));
-   }
-
-   return Promise.resolve();
+   return Promise.all(
+      langs.map(lang => parse(restaurant.menuUrl, lang))
+   )
+   .then(langMenus => {
+      return langs.reduce((carry, lang, i) => {
+         carry[lang] = langMenus[i];
+         return carry;
+      }, {})
+   })
+   .then(a => console.log(a));
+   // .then(menus => {
+   //    console.log('\tFound ' + menus.length + ' days of menues.');
+   //    return Promise.all(
+   //       menus.map(menu =>
+   //          models.Menu.findOne({
+   //             where: {
+   //                day: menu.day,
+   //                RestaurantId: restaurant.id
+   //             }
+   //          })
+   //          .then(existing => {
+   //             if (existing) {
+   //                return existing.update({
+   //                   courses_i18n: {
+   //                      ...existing.courses_i18n,
+   //                      [lang]: menu.courses
+   //                   }
+   //                });
+   //             }
+   //
+   //             return models.Menu.create({
+   //                day: menu.day,
+   //                RestaurantId: restaurant.id,
+   //                courses_i18n: {
+   //                   [lang]: menu.courses
+   //                }
+   //             });
+   //          })
+   //       )
+   //    );
+   // });
 }
 
 function updateAllRestaurants() {
+   let promiseChain = Promise.resolve();
    return models.Restaurant.findAll()
    .then(restaurants => {
       console.log('Start processing ' + restaurants.length + ' restaurants.\n');
-      return worker(restaurants);
+      restaurants.forEach(restaurant => {
+         promiseChain = promiseChain.then(() => {
+            console.log(`Processing ${restaurant.name_i18n.fi}:`);
+            return updateMenu(restaurant);
+         });
+      });
+      return promiseChain;
    });
 }
 
