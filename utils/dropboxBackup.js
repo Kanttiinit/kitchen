f4836d69d76b19b2c3fd760445afa0f7b146c935
Max Huttunen <max.huttunen@gmail.com>
2016-08-27 11:44:15 +0300

    exclude menus from backup

--- utils/dropboxBackup.js
+++ utils/dropboxBackup.js
@@ -4,7 +4,7 @@ const moment = require('moment');
 
 const dropbox = new Dropbox({accessToken: process.env.DROPBOX_TOKEN});
 
-const pg_dump = 'pg_dump ' + process.env.DATABASE_URL;
+const pg_dump = 'pg_dump -T menus ' + process.env.DATABASE_URL;
 
 exec(pg_dump, (error, contents, stderr) => {
   const path = '/' + moment().format('DD-MM-YYYY_HH-mm-ss');
