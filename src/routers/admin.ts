ffe5cb02ece36a122f134144acf7a5d738b3df6c
Max Huttunen <max.huttunen@gmail.com>
2017-08-08 10:59:37 +0300

    log in with admin password

--- src/routers/admin.ts
+++ src/routers/admin.ts
@@ -3,9 +3,13 @@ import * as worker from '../parser/worker';
 import * as models from '../models';
 import createModelRouter from '../utils/createModelRouter';
 import auth from '../utils/auth';
+import bufferEq from 'buffer-equal-constant-time';
+import * as crypto from 'crypto';
+
+const adminPassword = process.env.ADMIN_PASSWORD;
 
 export const verifyAdmin = (req, res, next) => {
-  if (req.user.admin) {
+  if (req.user.admin || req.session.admin) {
     next();
   } else {
     next({code: 401, message: 'Unauthorized.'});
@@ -26,7 +30,37 @@ export const updateRestaurants = async (req, res) => {
   res.json({message: 'ok'});
 };
 
-export default express.Router()
+export const login = (req, res) => {
+  const requestPassword = new Buffer(crypto.createHash('sha256').update(req.body.password).digest('base64'));
+  const password = new Buffer(adminPassword);
+  if (req.session.admin) {
+    res.json({message: 'Already logged in.'});
+  } else if (bufferEq(requestPassword, password)) {
+    req.session.admin = true;
+    res.json({message: 'Logged in.'});
+  } else {
+    res.status(401).json({message: 'Wrong password.'});
+  }
+};
+
+export const logout = (req, res) => {
+  if (req.session.admin) {
+    req.session.admin = false;
+    res.json({message: 'Logged out.'});
+  } else {
+    res.status(400).json({message: 'Not logged in.'});
+  }
+};
+
+const router = express.Router();
+
+if (adminPassword) {
+  router
+    .post('/login', login)
+    .post('/logout', logout);
+}
+
+router
 .use(auth)
 .use(verifyAdmin)
 .use(createModelRouter(models.Area))
@@ -34,3 +68,5 @@ export default express.Router()
 .use(createModelRouter(models.Favorite))
 .get('/update-area-maps', updateAreaMaps)
 .post('/update-restaurants', updateRestaurants);
+
+export default router;
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,9 @@
 import * as models from '../models';
 
 export default async (req, res, next) => {
-  if (req.session.user) {
+  if (req.session.admin) {
+    next();
+  } else if (req.session.user) {
     req.user = await models.User.findOne({where: {email: req.session.user}});
     next();
   } else {
