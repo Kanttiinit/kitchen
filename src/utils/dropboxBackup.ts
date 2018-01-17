ece2f9de94ffd60f1c6b5d6f37132069ae3cc13c
Max Huttunen <max.huttunen@gmail.com>
2018-01-17 23:55:32 +0200

    compress backups, script for importing backups

--- src/utils/dropboxBackup.ts
+++ src/utils/dropboxBackup.ts
@@ -1,17 +1,24 @@
 const Dropbox = require('dropbox');
 import * as moment from 'moment';
+import * as zlib from 'zlib';
+import {promisify} from 'util';
 import * as models from '../models';
 
 const dropbox = new Dropbox({accessToken: process.env.DROPBOX_TOKEN});
+
+const compress = (promisify as any)(zlib.deflate);
 
 models.sequelize.sync().then(async () => {
   const areas = await models.Area.findAll({raw: true});
   const restaurants = await models.Restaurant.findAll({raw: true});
   const favorites = await models.Favorite.findAll({raw: true});
   const path = '/' + moment().format('DD-MM-YYYY_HH-mm-ss');
+  const jsonString = JSON.stringify({areas, restaurants, favorites});
+  const compressedData = await compress(jsonString);
+
   await dropbox.filesUpload({
     path,
-    contents: JSON.stringify({areas, restaurants, favorites})
+    contents: compressedData.toString('base64')
   });
   models.sequelize.close();
   process.exit();
---
+++ src/utils/importBackup.ts
@@ -0,0 +1,47 @@
+import {readFileSync} from 'fs';
+import * as readline from 'readline';
+import {promisify} from 'util';
+import {unzip} from 'zlib';
+import {sequelize, Area, Restaurant, Favorite} from '../models';
+
+const decompress = (promisify as any)(unzip);
+
+async function importFromFile(filename) {
+  const fileContents = readFileSync(filename);
+  let data;
+  try {
+    const jsonString = await decompress(Buffer.from(fileContents.toString(), 'base64'));
+    data = JSON.parse(jsonString.toString());
+  } catch (e) {
+    throw new Error('Could not parse backup file: ' + e);
+  }
+  const rl = readline.createInterface({input: process.stdin, output: process.stdout});
+  rl.question(`About to import ${data.areas.length} areas, ${data.restaurants.length} restaurants, and ${data.favorites.length} favorites, continue? `, async answer => {
+    if (answer === 'Y' || answer === 'y') {
+      console.log('Importing...');
+      await importData(data);
+    } else {
+      console.log('Aborted.');
+    }
+    rl.close();
+  });
+}
+
+async function importData({areas, restaurants, favorites}) {
+  console.log('Connecting to DB.');
+  await sequelize.sync();
+  console.log('Importing areas.');
+  await Area.bulkCreate(areas);
+  console.log('Importing restaurants.');
+  await Restaurant.bulkCreate(restaurants);
+  console.log('Importing favorites.');
+  await Favorite.bulkCreate(favorites);
+  process.exit(0);
+}
+
+if (require.main === module) {
+  const filename = process.argv[2];
+  importFromFile(filename).catch(console.error);
+} else {
+  throw new Error('This file is not meant to be imported.');
+}
