0e788ab6067164b48899f4d659ea0760d822e570
Max Huttunen <max.huttunen@gmail.com>
2018-05-31 16:59:03 +0300

    add migration script

---
+++ migrations/openingHours.js
@@ -0,0 +1,35 @@
+const moment = require('moment');
+const models = require('../dist/models');
+
+const formatTime = time => {
+  const str = String(time);
+  return str.substr(0, 2) + ':' + str.substr(2);
+};
+
+(async () => {
+  await models.sequelize.sync();
+  const restaurants = await models.Restaurant.findAll();
+  const from = moment().format('YYYY-MM-DD');
+  for (const restaurant of restaurants) {
+    const openingHours = [];
+    if (restaurant.openingHours && restaurant.openingHours.length) {
+      let dayOfWeek = 0;
+      for (const hours of restaurant.openingHours) {
+        if (hours) {
+          const [opens, closes] = hours;
+          openingHours.push({
+            opens: formatTime(opens),
+            closes: formatTime(closes),
+            manualEntry: false,
+            from,
+            dayOfWeek,
+            RestaurantId: restaurant.id
+          });
+        }
+        dayOfWeek++;
+      }
+    }
+    // console.log(openingHours);
+    models.OpeningHours.bulkCreate(openingHours);
+  }
+})();
