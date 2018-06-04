e56c6bddc5e1172a3fcbf0589d88d59674a3730c
Max Huttunen <max.huttunen@gmail.com>
2018-06-04 20:29:53 +0300

    fix dropbox

--- src/utils/dropboxBackup.ts
+++ src/utils/dropboxBackup.ts
@@ -1,20 +1,20 @@
-const Dropbox = require('dropbox');
+import { Dropbox } from 'dropbox';
 import * as moment from 'moment';
 import * as zlib from 'zlib';
-import {promisify} from 'util';
+import { promisify } from 'util';
 import * as models from '../models';
 
-const dropbox = new Dropbox({accessToken: process.env.DROPBOX_TOKEN});
+const dropbox = new Dropbox({ accessToken: process.env.DROPBOX_TOKEN });
 
 const compress = (promisify as any)(zlib.deflate);
 
 models.sequelize.sync().then(async () => {
   while (true) {
-    const areas = await models.Area.findAll({raw: true});
-    const restaurants = await models.Restaurant.findAll({raw: true});
-    const favorites = await models.Favorite.findAll({raw: true});
+    const areas = await models.Area.findAll({ raw: true });
+    const restaurants = await models.Restaurant.findAll({ raw: true });
+    const favorites = await models.Favorite.findAll({ raw: true });
     const path = '/' + moment().format('DD-MM-YYYY_HH-mm-ss');
-    const jsonString = JSON.stringify({areas, restaurants, favorites});
+    const jsonString = JSON.stringify({ areas, restaurants, favorites });
     const compressedData = await compress(jsonString);
 
     await dropbox.filesUpload({
