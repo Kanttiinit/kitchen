f8aa1a30d9b1a94c493e427a7510a1bc5bd4af99
Max Huttunen <max.huttunen@gmail.com>
2018-08-30 16:17:39 +0300

    fix kipsari parser

--- src/menu-parser/parsers/kipsari.ts
+++ src/menu-parser/parsers/kipsari.ts
@@ -17,12 +17,12 @@ const parser: Parser = {
     const xml = await parseXml(await text(url));
     const items = xml.rss.channel[0].item;
     return items.map(({ title, description }) => {
-      const date = moment(title[0].split(', ')[1]).format('YYYY-MM-DD');
+      const day = moment(title[0].split(', ')[1]).format('YYYY-MM-DD');
       const { document } = new JSDOM(description[0], {
         features: { QuerySelector: true }
       }).window;
       return {
-        date,
+        day,
         courses: (Array.from(document.querySelectorAll('span')) as any).map(
           course => {
             const match = course.textContent.trim().match(/\(.+\)$/gi);
