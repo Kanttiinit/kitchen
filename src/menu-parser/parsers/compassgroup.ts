1ba4dde5be8e67b11a5c5c81664dddded3151a8b
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-02-26 22:36:32 +0200

    Add title and property parser for food courses

--- src/menu-parser/parsers/compassgroup.ts
+++ src/menu-parser/parsers/compassgroup.ts
@@ -12,11 +12,38 @@ import {
 const dateRegExp = /(\d+).(\d+)./g;
 
 const parseToDate = (possiblyDate: string): string | null => {
-    const match = dateRegExp.exec(possiblyDate);
-    return match
-        ? moment(match[0], "DD.M").format('YYYY-MM-DD')
-        : null;
-}
+  const match = dateRegExp.exec(possiblyDate);
+  return match
+    ? moment(match[0], 'DD.M').format('YYYY-MM-DD')
+    : null;
+};
+const propertyMap = {
+  PÄ: Property.CONTAINS_ALLERGENS,
+  VS: Property.CONTAINS_ALLERGENS,
+  G: Property.GLUTEN_FREE,
+  V: Property.VEGAN,
+  L: Property.LACTOSE_FREE,
+  M: Property.MILK_FREE,
+  VL: Property.LOW_IN_LACTOSE
+};
+
+const normalizeProperties = createPropertyNormalizer(propertyMap);
+
+interface Course {
+  title: string;
+  properties: Array<Property>;
+};
+
+const parseToCourse = (possiblyCourse: string): Course | null => {
+  if (possiblyCourse.trim() === '') {
+    return null;
+  }
+  const [rawTitle, rawProperties] = possiblyCourse.split(' (');
+  return {
+    title: rawTitle.trim(),
+    properties: normalizeProperties(rawProperties.match(propertyRegex))
+  };
+};
 
 const parser: Parser = {
   pattern: /compassgroup/,
@@ -27,16 +54,26 @@ const parser: Parser = {
     const container = document.querySelector('#mid-column');
     const elements = [...container.querySelectorAll('p')];
 
-    return Array.from(elements.reduce((acc: any, curr: any) => {
-        const rowAsDate = parseToDate(curr.textContent);
-        if (rowAsDate) {
-            acc.push({
-                day: rowAsDate,
-                courses: Array.from([])
-            })
-        }
-        return acc;
-    }, []))
+    const indexOfFirstNotCourseElement = elements.findIndex(
+      e => e.textContent.includes('Muutokset ovat mahdollisia')
+    );
+
+    const courseElements = elements.slice(0, indexOfFirstNotCourseElement - 1);
+
+    return Array.from(courseElements.reduce((acc: any, curr: any) => {
+      const rowAsDate = parseToDate(curr.textContent);
+      if (rowAsDate) {
+        return [...acc, {
+          day: rowAsDate,
+          courses: Array.from([])
+        }];
+      }
+      const rowAsCourse = parseToCourse(curr.textContent);
+      if (rowAsCourse) {
+        acc[acc.length - 1].courses.push(rowAsCourse);
+      }
+      return acc;
+    }, []));
   }
 };
 
