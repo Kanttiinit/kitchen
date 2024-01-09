9c9effa608d12fd1c6fd504463287d59f2b05c63
max <max.huttunen@gmail.com>
2024-01-09 17:48:40 +0200

    fix kipsari warning

--- src/menu-parser/parsers/kipsari.ts
+++ src/menu-parser/parsers/kipsari.ts
@@ -30,7 +30,7 @@ const parser: Parser = {
     const xml = await parseXml(await text(url));
     const items = xml.rss.channel[0].item;
     return items.map(({ title, description }) => {
-      const day = moment(title[0].split(', ')[1]).format('YYYY-MM-DD');
+      const day = moment(title[0].split(', ')[1], 'DD.MM.YYYY').format('YYYY-MM-DD');
       const { document } = new JSDOM(description[0], {
         features: { QuerySelector: true }
       }).window;
