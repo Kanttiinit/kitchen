0bda506f3aa1c0404e5ea4174cf41543d722a19f
aleksi <aleksi-2@MacBook-Pro-2.local>
2025-11-08 22:49:30 +0200

    fix: remove test urls that are no longer updated

--- test/parsers.test.js
+++ test/parsers.test.js
@@ -9,15 +9,10 @@ async function expectCorrectFormat(url, lang) {
 }
 
 const urls = [
-  'http://messi.hyyravintolat.fi/publicapi/restaurant/4',
-  'http://api.teknolog.fi/taffa/fi/html/week/',
-  'http://www.sodexo.fi/ruokalistat/output/weekly_json/142/%year%/%month%/%day%/fi',
-  'http://www.amica.fi/modules/json/json/Index?costNumber=3579&language=fi&firstDay=%year%-%month%-%day%',
-  'http://www.hys.net/ruokalista.xml',
-  'http://www.mau-kas.fi/ravintola.html?listtype=lunch&showall=true'
+  'http://www.hys.net/ruokalista.xml'
 ];
 
-describe.skip('Parsers', () => {
+describe('Parsers', () => {
   describe('restaurant', () => {
     urls.forEach(url => {
       describe('parser for ' + url.split('/')[2], function() {
