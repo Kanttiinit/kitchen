62b8605dd7221bb6df9d61a4505da372688164ee
max <max.huttunen@gmail.com>
2026-09-02 12:36:55 +0300

    wip

--- src/environment.ts
+++ src/environment.ts
@@ -1,14 +1,8 @@
-export const sessionSecret = process.env.SESSION_SECRET;
 export const origins = process.env.ORIGINS ? process.env.ORIGINS.split(',') : [];
 export const isProduction = process.env.NODE_ENV === 'production';
 export const isTest = process.env.NODE_ENV === 'test';
-export const universalAnalyticsId = process.env.UA_ID;
-export const adminPassword = process.env.ADMIN_PASSWORD;
 export const port = process.env.PORT || 3000;
 export const databaseURL = process.env.DATABASE_URL;
-export const sequelizeLogging = Boolean(Number(process.env.SEQUELIZE_LOGGING));
 export const telegramModeratorChatId = Number(process.env.TG_CHAT_ID);
 export const telegramFeedbackChatId = Number(process.env.TG_FEEDBACK_CHAT_ID);
 export const telegramBotToken = process.env.BOT_TOKEN;
-export const dropboxToken = process.env.DROPBOX_TOKEN;
-export const sentryDSN = process.env.SENTRY_DSN;
